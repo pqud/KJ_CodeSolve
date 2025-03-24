91a7cc90b6d0e103159ce0ccb87ed93c963165e9
pqud <49116761+pqud@users.noreply.github.com>
2025-03-24 13:49:15 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 184 ms, Memory: 140748 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -1,39 +1,27 @@
 import sys
 
-def largestRectangleArea(heights):
-    n = len(heights)
-    if n == 0:
-        return 0
-    
-    nodes = [[h, i, i, ''] for i, h in enumerate(heights)]  # [높이, l, r, rl]
-    max_area = 0
-    
-    # Step 1: rl 값 설정
-    for i in range(1, n-1):
-        if heights[i-1] > heights[i+1]:
-            nodes[i][3] = 'l'
-        else:
-            nodes[i][3] = 'r'
-    
-    # Step 2: l과 r 설정 (왼쪽/오른쪽 확장 범위)
-    for i in range(n):
-        while nodes[i][1] > 0 and heights[nodes[i][1] - 1] >= nodes[i][0]:
-            nodes[i][1] = nodes[nodes[i][1] - 1][1]  # 왼쪽 확장
-    
-    for i in range(n-1, -1, -1):
-        while nodes[i][2] < n-1 and heights[nodes[i][2] + 1] >= nodes[i][0]:
-            nodes[i][2] = nodes[nodes[i][2] + 1][2]  # 오른쪽 확장
-    
-    # Step 3: 병합하면서 최대 넓이 갱신
-    for i in range(n):
-        width = nodes[i][2] - nodes[i][1] + 1
-        max_area = max(max_area, width * nodes[i][0])
-    
-    return max_area
+def largestRectangleArea(heights, L):
+    stack = []  # (인덱스, 높이)
+    max_size = 0
 
-# 예제 실행
+    for i in range(L):
+        while stack and stack[-1][1] > heights[i]: #스택 꼭대기의 블록의 높이가 새 블록보다 크면면
+            top = stack.pop()
+            width = i if not stack else i - stack[-1][0] - 1 #가로길이는 스택이 비지 않았다면 i 스택이 비었으면 i-꼭대기 블록의 인덱스  
+            max_size = max(max_size, top[1] * width) 
+        stack.append((i, heights[i]))
+
+    while stack: #스택이 빌 때 까지(위의 루프 다 돌고나서 아직 계산하지 않은 히스토그램 있음)
+        top = stack.pop() 
+        width = L if not stack else L - stack[-1][0] - 1 #가로길이는 스택이 비지 않았다면 L 비었으면 L-꼭대기 블록의 인덱스
+        max_size = max(max_size, top[1] * width)
+
+    return max_size
+
 while True:
-    heights=list(map(int, sys.stdin.readline().split()))
-    if heights[0]==0:
+    heights = list(map(int, sys.stdin.readline().split()))
+    if heights[0] == 0:
         break
-    print(largestRectangleArea(heights[1:]))  # 결과: 10
+
+    result = largestRectangleArea(heights[1:], heights[0])
+    print(result)
