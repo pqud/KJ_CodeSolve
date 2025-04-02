a624e6dba1e9304abc327ca199c117588cc954cf
pqud <49116761+pqud@users.noreply.github.com>
2025-04-02 10:46:01 +0900

    [Gold V] Title: 동전 2, Time: 124 ms, Memory: 111308 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -10,26 +10,14 @@ coins=[]
 for _ in range(n):
     coins.append(int(input()))
 
-def mincoin_dfs(target):
-    queue=deque()
-    queue.append((0,0)) #(총합, 현재 코인 개수) 가 쌍으로 들어감
+dp=[float('inf')] * (m+1)
+dp[0]=0
 
-    visited=set()
-    visited.add((0,0))
+for coin in coins:
+    for i in range(coin, m+1) :
+        dp[i]=min(dp[i], dp[i-coin]+1)
 
-    while queue:
-        cur_hap, coin_count = queue.popleft()
-
-        if cur_hap==target:
-            return coin_count
-        
-        for coin in coins:
-            next_hap = cur_hap+coin
-            if next_hap <=target and next_hap not in visited:
-                visited.add(next_hap)
-                queue.append((next_hap, coin_count+1))
-
-    return -1
-
-print(mincoin_dfs(m))
-
+if dp[m]==float('inf'):
+    print(-1)
+else:
+    print(dp[m])
